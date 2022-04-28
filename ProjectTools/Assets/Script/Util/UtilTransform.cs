9895f9743fe2ad4a04af0f694a322dd908448b97
liguangming <liguangminglq@163.com>
2022-04-28 11:02:57 +0800

    【3D转2D坐标，防止背向投影】

---
+++ ProjectTools/Assets/Script/Util/UtilTransform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ETModel
+{
+    public class UtilTransform
+    {
+        public static Vector2 WorldToScreenPointProjected( Camera camera, Vector3 worldPos )
+        {
+            if (camera == null) return Vector2.zero;
+            Vector3 camNormal = camera.transform.forward;
+            Vector3 vectorFromCam = worldPos - camera.transform.position;
+            float camNormDot = Vector3.Dot( camNormal, vectorFromCam.normalized);
+            if ( camNormDot <= 0 )
+            {
+                float camDot = Vector3.Dot(camNormal, vectorFromCam);
+                Vector3 proj = ( camNormal * camDot * 1.01f );
+                worldPos = camera.transform.position + ( vectorFromCam - proj );
+            }
+            return RectTransformUtility.WorldToScreenPoint( camera, worldPos );
+        }
+
+    }
+}
